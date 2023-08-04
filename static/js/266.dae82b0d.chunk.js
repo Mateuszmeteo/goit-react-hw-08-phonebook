99fa1629847b9e0845141956053bb5cb7c8e48c5
Mateuszmeteo <44093947+Mateuszmeteo@users.noreply.github.com>
2023-08-04 11:49:07 +0000

    Deploying to gh-pages from @ Mateuszmeteo/goit-react-hw-08-phonebook@1e67bb7cef502fcc9a0b00ed6d03adaa38a1561d 🚀

---
+++ static/js/266.dae82b0d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{266:function(e,_,o){o.r(_),o.d(_,{default:function(){return l}});var h="home_home__DdOkd",s="home_home__div__xUEKp",t="home_home__title__WCyNk",c="home_home__subtitle__-PU9k",a=o(329),l=function(){return(0,a.jsx)("div",{className:h,children:(0,a.jsxs)("div",{className:s,children:[(0,a.jsx)("h1",{className:t,children:"Welcome!"}),(0,a.jsx)("p",{className:c,children:"PhoneBook Web Application"})]})})}}}]);
+//# sourceMappingURL=266.dae82b0d.chunk.js.map
